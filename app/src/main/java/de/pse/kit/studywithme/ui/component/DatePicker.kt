d957a60b662a190bfbded47038545d7f8d3fe4d5
Kaan Göbül <shelduar99@MacBook-Pro-von-uongz.fritz.box>
2022-01-22 19:48:05 +0100

    Changed Design of DatePicker

--- app/src/main/java/de/pse/kit/studywithme/ui/component/DatePicker.kt
+++ app/src/main/java/de/pse/kit/studywithme/ui/component/DatePicker.kt
@@ -2,18 +2,31 @@ package de.pse.kit.studywithme.ui.component
 
 import android.app.DatePickerDialog
 import android.content.Context
+import android.os.Build
 import android.widget.DatePicker
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import de.pse.kit.myapplication.ui.theme.MyApplicationTheme
+import de.pse.kit.myapplication.ui.theme.MyApplicationTheme3
 import java.util.*
 
 
@@ -37,20 +50,32 @@ fun DatePicker(context: Context) {
         }, year, month, day
     )
 
-    Column(
-        modifier = Modifier.fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Datum einfügen: ${date.value}")
-        Spacer(modifier = Modifier.size(16.dp))
-        Button(onClick = {
-            datePickerDialog.show()
-        }) {
-            Text(text = "Öffne Date Picker")
+    MyApplicationTheme3() {
+        var expanded by remember { mutableStateOf(false) }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.TopStart)
+        ) {
+            Text(
+                date.value, modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(onClick = { datePickerDialog.show() })
+                    .background(MaterialTheme.colorScheme.surface)
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface)
+            ) {
+                datePickerDialog.show()
+            }
         }
     }
 }
+
 
 @Preview
 @Composable
