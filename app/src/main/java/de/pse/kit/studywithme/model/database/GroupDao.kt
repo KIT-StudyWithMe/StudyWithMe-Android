a00fabc81fc57ce3bc76404b512e2c155ae4bf49
Jonathan Bonkosch <jbonkosch@yahoo.de>
2022-01-25 16:58:48 +0100

    Correct GroupDao

--- app/src/main/java/de/pse/kit/studywithme/model/database/GroupDao.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/database/GroupDao.kt
@@ -6,13 +6,17 @@ import de.pse.kit.studywithme.model.data.Group
 @Dao
 interface GroupDao {
     @Query("SELECT * FROM `group`")
-    fun getGroups(userID: Int)
+    fun getGroups()
+
     @Query("SELECT * FROM `group` WHERE group_ID LIKE :groupID" )
     fun getGroup(groupID: Int)
+
     @Insert
     fun saveGroup(group: Group) {}
+
     @Update
     fun editGroup(group: Group) {}
+
     @Delete
     fun removeGroup(group: Group){}
 }
