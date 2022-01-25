17d66f903052d93d5dbbf7e40bcecccbc4f3b235
urkxw <jonas.kaiser@student.kit.edu>
2022-01-25 17:29:32 +0100

    Update GroupDao.kt

--- app/src/main/java/de/pse/kit/studywithme/model/database/GroupDao.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/database/GroupDao.kt
@@ -1,18 +1,26 @@
 package de.pse.kit.studywithme.model.database
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import de.pse.kit.studywithme.model.data.Group
 
 @Dao
 interface GroupDao {
     @Query("SELECT * FROM `group`")
     fun getGroups(userID: Int)
+
     @Query("SELECT * FROM `group` WHERE group_ID LIKE :groupID" )
     fun getGroup(groupID: Int)
+
     @Insert
-    fun saveGroup(group: Group) {}
+    fun saveGroup(group: Group)
+
     @Update
-    fun editGroup(group: Group) {}
+    fun editGroup(group: Group)
+
     @Delete
-    fun removeGroup(group: Group){}
+    fun removeGroup(group: Group)
 }
