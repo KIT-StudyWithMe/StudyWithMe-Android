afff8fb8d54c849b1ae270855d0bf1059ab921a6
urkxw <jonas.kaiser@student.kit.edu>
2022-01-25 17:29:18 +0100

    Update AppDatabase.kt

--- app/src/main/java/de/pse/kit/studywithme/model/database/AppDatabase.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/database/AppDatabase.kt
@@ -2,9 +2,12 @@ package de.pse.kit.studywithme.model.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import de.pse.kit.studywithme.model.data.Session
+import de.pse.kit.studywithme.model.data.*
 
-@Database(entities = [Session::class], version = 1)
+@Database(entities = [Session::class, Group::class, Lecture::class, User::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionDao
+    abstract fun groupDao(): GroupDao
+    abstract fun lectureDao(): LectureDao
+    abstract fun userDao(): UserDao
 }
