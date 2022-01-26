b39e2c31d45c5317a2c77ee689cc14eb45c24eea
urkxw <jonas.kaiser@student.kit.edu>
2022-01-26 15:03:30 +0100

    Add TypeConverters to persist Date objects

--- app/src/main/java/de/pse/kit/studywithme/model/database/AppDatabase.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/database/AppDatabase.kt
@@ -2,9 +2,12 @@ package de.pse.kit.studywithme.model.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import de.pse.kit.studywithme.model.data.*
+import de.pse.kit.studywithme.model.database.TypeConverters as TypeConverters_
 
 @Database(entities = [Session::class, Group::class, Lecture::class, User::class], version = 1)
+@TypeConverters(TypeConverters_::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionDao
     abstract fun groupDao(): GroupDao
