01e02fe070719bb2b4675603d0c7413a1652ec40
uongz <uongz@student.kit.edu>
2022-02-02 21:25:16 +0100

    Add LectureRepository

---
+++ app/src/main/java/de/pse/kit/studywithme/model/repository/LectureRepository.kt
@@ -0,0 +1,24 @@
+package de.pse.kit.studywithme.model.repository
+
+import android.content.Context
+import de.pse.kit.studywithme.model.data.Lecture
+import de.pse.kit.studywithme.model.network.LectureService
+import kotlinx.coroutines.runBlocking
+
+class LectureRepository private constructor(context: Context) {
+    private val lectureService = LectureService.instance
+
+    fun getLectures(prefix: String): List<Lecture> {
+        return runBlocking {
+            return@runBlocking lectureService!!.getLectures(prefix)
+        }
+    }
+
+    fun saveLecture(lecture: Lecture) {
+        //TODO
+    }
+
+    fun removeLecture(lectureID: Int) {
+        //TODO
+    }
+}
