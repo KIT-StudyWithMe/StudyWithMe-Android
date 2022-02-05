a6e088d831a361cd7aeecb0709539ae48e28c833
uongz <uongz@student.kit.edu>
2022-02-05 19:36:43 +0100

    Update DatePicker.kt

--- app/src/main/java/de/pse/kit/studywithme/ui/component/DatePicker.kt
+++ app/src/main/java/de/pse/kit/studywithme/ui/component/DatePicker.kt
@@ -2,30 +2,17 @@ package de.pse.kit.studywithme.ui.component
 
 import android.app.DatePickerDialog
 import android.content.Context
-import android.os.Build
 import android.widget.DatePicker
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import de.pse.kit.myapplication.ui.theme.MyApplicationTheme
 import de.pse.kit.myapplication.ui.theme.MyApplicationTheme3
 import java.util.*
 
@@ -35,6 +22,7 @@ fun DatePicker(context: Context) {
     val year: Int
     val month: Int
     val day: Int
+    val color: Int = 4
 
     val calendar = Calendar.getInstance()
     year = calendar.get(Calendar.YEAR)
@@ -45,35 +33,28 @@ fun DatePicker(context: Context) {
     val date = remember { mutableStateOf("") }
     val datePickerDialog = DatePickerDialog(
         context,
-        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
-            date.value = "$dayOfMonth/$month/$year"
-        }, year, month, day
+        color,
+        { _: DatePicker, year, month, dayOfMonth ->
+            date.value = "$dayOfMonth/${month+1}/$year"
+        },
+        year,
+        month,
+        day
     )
 
     MyApplicationTheme3() {
-        var expanded by remember { mutableStateOf(false) }
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .wrapContentSize(Alignment.TopStart)
+                .background(MaterialTheme.colorScheme.background)
         ) {
             Text(
                 date.value, modifier = Modifier
                     .fillMaxWidth()
                     .clickable(onClick = { datePickerDialog.show() })
-                    .background(MaterialTheme.colorScheme.surface)
+                    .background(MaterialTheme.colorScheme.primary)
             )
-            /*
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(MaterialTheme.colorScheme.surface)
-            ) {
-                datePickerDialog.show()
-            }
-            */
         }
     }
 }
