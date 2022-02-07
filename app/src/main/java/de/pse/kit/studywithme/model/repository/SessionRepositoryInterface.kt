2f731d7ade7d67db3dd63b89233e7f7b82df567e
uongz <uongz@student.kit.edu>
2022-02-07 21:23:28 +0100

    Add FakeSessionRepo & SessionRepoInterface

---
+++ app/src/main/java/de/pse/kit/studywithme/model/repository/SessionRepositoryInterface.kt
@@ -0,0 +1,24 @@
+package de.pse.kit.studywithme.model.repository
+
+import de.pse.kit.studywithme.model.data.Session
+import de.pse.kit.studywithme.model.data.SessionAttendee
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+interface SessionRepositoryInterface {
+
+    @ExperimentalCoroutinesApi
+    fun getSessions(groupID: Int): Flow<List<Session>>
+
+    fun newSession(session: Session): Boolean
+
+    fun editSession(session: Session): Boolean
+
+    fun removeSession(session: Session)
+
+    fun newAttendee(sessionID: Int): Boolean
+
+    fun removeAttendee(sessionID: Int)
+
+    fun getAttendees(sessionID: Int): Flow<List<SessionAttendee>>
+}
