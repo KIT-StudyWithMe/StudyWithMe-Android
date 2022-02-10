361dea5fa30db377dc134fa47dbf96956c847185
Jonathan Bonkosch <jbonkosch@yahoo.de>
2022-02-10 13:26:49 +0100

    Update GroupService

--- app/src/main/java/de/pse/kit/studywithme/model/network/GroupService.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/network/GroupService.kt
@@ -7,20 +7,20 @@ import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 
 interface GroupService {
-    suspend fun getGroups(search: String): List<RemoteGroup>
-    suspend fun getJoinedGroups(uid: Int): List<RemoteGroup>
-    suspend fun getGroupSuggestions(uid: Int): List<RemoteGroup>
+    suspend fun getGroups(search: String): List<RemoteGroup>?
+    suspend fun getJoinedGroups(uid: Int): List<RemoteGroup>?
+    suspend fun getGroupSuggestions(uid: Int): List<RemoteGroup>?
     suspend fun getGroup(groupID: Int): RemoteGroup?
     suspend fun getGroupMembers(groupID: Int): List<GroupMember>?
     suspend fun newGroup(group: RemoteGroup): RemoteGroup?
     suspend fun editGroup(groupID: Int, group: RemoteGroup): RemoteGroup?
-    suspend fun removeGroup(groupID: Int)
+    suspend fun removeGroup(groupID: Int): Boolean
     suspend fun newMember(groupID: Int, uid: Int): GroupMember?
-    suspend fun joinRequest(groupID: Int, uid:Int)
-    suspend fun removeMember(groupID: Int, uid: Int)
-    suspend fun getLectures(prefix: String): List<Lecture>
+    suspend fun joinRequest(groupID: Int, uid:Int): Boolean
+    suspend fun removeMember(groupID: Int, uid: Int): Boolean
+    suspend fun getLectures(prefix: String): List<Lecture>?
     suspend fun getLecture(lectureID: Int): Lecture?
-    suspend fun newLecture(lecture: Lecture): Lecture?
+    suspend fun newLecture(lecture: Lecture, groupID: Int): Lecture?
     suspend fun getMajor(majorID: Int): Major?
 
     companion object {
--- app/src/main/java/de/pse/kit/studywithme/model/network/GroupServiceImpl.kt
+++ app/src/main/java/de/pse/kit/studywithme/model/network/GroupServiceImpl.kt
@@ -7,23 +7,23 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 
 class GroupServiceImpl(private var client: HttpClient): GroupService {
-    override suspend fun getGroups(search: String): List<RemoteGroup> {
+    override suspend fun getGroups(search: String): List<RemoteGroup>? {
         return try {
             client.get(HttpRoutes.GROUPS) {
                 parameter("text", search)
             }
         } catch (e: RedirectResponseException) {
             println("Redirect Error: ${e.response.status.description}")
-            emptyList<RemoteGroup>()
+            null
         } catch (e: ClientRequestException) {
             println("Request Error: ${e.response.status.description}")
-            emptyList<RemoteGroup>()
+            null
         } catch (e: ServerResponseException) {
             println("Response Error: ${e.response.status.description}")
-            emptyList<RemoteGroup>()
+            null
         } catch (e: Exception) {
             println("Error: ${e.message}")
-            emptyList<RemoteGroup>()
+            null
         }
     }
 
@@ -73,12 +73,8 @@ class GroupServiceImpl(private var client: HttpClient): GroupService {
     }
 
     override suspend fun newGroup(group: RemoteGroup): RemoteGroup? {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun editGroup(groupID: Int, group: RemoteGroup): RemoteGroup? {
         return try {
-            client.put(HttpRoutes.GROUPS + groupID + "/detail") {
+            client.post(HttpRoutes.GROUPS) {
                 contentType(ContentType.Application.Json)
                 body = group
             }
@@ -97,30 +93,83 @@ class GroupServiceImpl(private var client: HttpClient): GroupService {
         }
     }
 
-    override suspend fun removeGroup(groupID: Int) {
-        TODO("Not yet implemented")
+    override suspend fun editGroup(groupID: Int, group: RemoteGroup): RemoteGroup? {
+        return try {
+            client.put(HttpRoutes.GROUPS + groupID) {
+                contentType(ContentType.Application.Json)
+                body = group
+            }
+        } catch (e: RedirectResponseException) {
+            println("Redirect Error: ${e.response.status.description}")
+            null
+        } catch (e: ClientRequestException) {
+            println("Request Error: ${e.response.status.description}")
+            null
+        } catch (e: ServerResponseException) {
+            println("Response Error: ${e.response.status.description}")
+            null
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            null
+        }
+    }
+
+    override suspend fun removeGroup(groupID: Int): Boolean {
+        return try {
+            client.delete(HttpRoutes.GROUPS + groupID)
+        } catch (e: RedirectResponseException) {
+            println("Redirect Error: ${e.response.status.description}")
+            false
+        } catch (e: ClientRequestException) {
+            println("Request Error: ${e.response.status.description}")
+            false
+        } catch (e: ServerResponseException) {
+            println("Response Error: ${e.response.status.description}")
+            false
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            false
+        }
     }
 
     override suspend fun newMember(groupID: Int, uid: Int): GroupMember? {
         TODO("Not yet implemented")
     }
 
-    override suspend fun joinRequest(groupID: Int, uid: Int) {
-        try {
+    override suspend fun joinRequest(groupID: Int, uid: Int): Boolean {
+        return try {
             client.put(HttpRoutes.GROUPS + groupID + "/join/" + uid)
         } catch (e: RedirectResponseException) {
             println("Redirect Error: ${e.response.status.description}")
+            false
         } catch (e: ClientRequestException) {
             println("Request Error: ${e.response.status.description}")
+            false
         } catch (e: ServerResponseException) {
             println("Response Error: ${e.response.status.description}")
+            false
         } catch (e: Exception) {
             println("Error: ${e.message}")
+            false
         }
     }
 
-    override suspend fun removeMember(groupID: Int, uid: Int) {
-        TODO("Not yet implemented")
+    override suspend fun removeMember(groupID: Int, uid: Int): Boolean {
+        return try {
+            client.delete(HttpRoutes.GROUPS + "/users/" + uid)
+        } catch (e: RedirectResponseException) {
+            println("Redirect Error: ${e.response.status.description}")
+            false
+        } catch (e: ClientRequestException) {
+            println("Request Error: ${e.response.status.description}")
+            false
+        } catch (e: ServerResponseException) {
+            println("Response Error: ${e.response.status.description}")
+            false
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            false
+        }
     }
 
     override suspend fun getLectures(prefix: String): List<Lecture> {
@@ -131,8 +180,25 @@ class GroupServiceImpl(private var client: HttpClient): GroupService {
         TODO("Not yet implemented")
     }
 
-    override suspend fun newLecture(lecture: Lecture): Lecture? {
-        TODO("Not yet implemented")
+    override suspend fun newLecture(lecture: Lecture, groupID: Int): Lecture? {
+        return try {
+            client.post(HttpRoutes.GROUPS + groupID + "/sessions") {
+                contentType(ContentType.Application.Json)
+                body = lecture
+            }
+        } catch (e: RedirectResponseException) {
+            println("Redirect Error: ${e.response.status.description}")
+            null
+        } catch (e: ClientRequestException) {
+            println("Request Error: ${e.response.status.description}")
+            null
+        } catch (e: ServerResponseException) {
+            println("Response Error: ${e.response.status.description}")
+            null
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+            null
+        }
     }
 
     override suspend fun getMajor(majorID: Int): Major? {
