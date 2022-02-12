49ef752c646460b6da3a5394d1e8e12e3bbe8d14
uongz <uongz@student.kit.edu>
2022-02-12 10:26:27 +0100

    Add JDOC SignedInVM & VM

--- app/src/main/java/de/pse/kit/studywithme/viewModel/ViewModel.kt
+++ app/src/main/java/de/pse/kit/studywithme/viewModel/ViewModel.kt
@@ -4,6 +4,12 @@ package de.pse.kit.studywithme.viewModel
 import androidx.lifecycle.ViewModel as ViewModel_
 import androidx.navigation.NavController
 
+/**
+ * Implementation of backward navigation
+ *
+ * @property navController
+ * @constructor Create empty View model
+ */
 open class ViewModel(val navController: NavController)/*: ViewModel_()*/ {
     fun navBack() {
         navController.popBackStack()
